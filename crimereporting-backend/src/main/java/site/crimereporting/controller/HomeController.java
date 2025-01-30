f2152bbc4cff8b90ef8e0a125f07aaf67005af4d
Lalini <lalinisahusirri@gmail.com>
2025-01-30 18:52:27 +0530

    SMTP problem resolved

--- crimereporting-backend/src/main/java/site/crimereporting/controller/HomeController.java
+++ crimereporting-backend/src/main/java/site/crimereporting/controller/HomeController.java
@@ -1,16 +1,15 @@
 package site.crimereporting.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import site.crimereporting.dtos.ApiResponse;
-import site.crimereporting.entity.User;
-
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import site.crimereporting.dtos.ApiResponse;
 
 @RestController
 
