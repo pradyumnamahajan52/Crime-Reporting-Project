8df15b8d39a63191df9f958b000409a56041bda1
Mitali Gupta <mitaligupta141@gmail.com>
2025-01-31 15:53:01 +0530

    updated

---
+++ client/src/data/crime_category.js
@@ -0,0 +1,81 @@
+import image1 from '../assets/images/crime-categories/crime1.png'
+import image2 from '../assets/images/crime-categories/crime2.png'
+import image3 from '../assets/images/crime-categories/crime3.png'
+import image4 from '../assets/images/crime-categories/crime4.png'
+
+
+const category =[
+    {
+      id: 1,
+      text:"Property crimes",
+    
+      img:image1
+    },
+    {
+      id: 2,
+      text:"Violent Crimes",
+      img:image1
+    },
+    {
+      id: 3,
+      text:" Cybercrimes",
+    
+      img:image1
+    },
+    {
+      id: 4,
+      text:"White Collar Crimes",
+     
+      img:image1
+    },
+    {
+        id: 5,
+        text:"Drug-Related Crimes",
+       
+        img:image1
+      },
+      {
+        id: 6,
+        text:"Motor Vehicle Offenses",
+      
+        img:image1
+      },
+      {
+        id: 7,
+        text:"Public Order Crimes",
+      
+        img:image1
+      },
+      {
+        id: 8,
+        text:"Environmental Crimes",
+       
+        img:image1
+      },
+      {
+        id: 9,
+        text:"Fraud & Financial Crimes",
+       
+        img:image1
+      },
+      {
+        id: 10,
+        text:"Crimes Against Govt. ",
+        
+        img:image1
+      },
+      {
+        id: 11,
+        text:"Hate Crimes",
+       
+        img:image1
+      },
+      {
+        id: 12,
+        text:"Miscellaneous Crimes",
+        img:image1
+      },
+    
+  ]
+
+  export default category
