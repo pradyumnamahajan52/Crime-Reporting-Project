4babb7e9e9a9d46a2806cf746aa8a58cf1ab1224
Pawan Kumar Gupta <pawankumarg6294@gmail.com>
2025-01-31 09:08:23 +0530

    added police panel layout

---
+++ client/src/Layout/PoliceLayout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Sidebar from "../Components/Police/Sidebar";
+import Dashboard from "../Screens/Police/Dashboard";
+function App() {
+  return (
+    <div className="flex">
+      <Sidebar />
+      {/* <Dashboard /> */}
+    </div>
+  );
+}
+
+export default App;
