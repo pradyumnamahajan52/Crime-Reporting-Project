104a0162ab22a220a86b31a0163e81334afb83ba
pradyumnamahajan52 <pradyumnamahajan52@gmail.com>
2025-02-09 23:41:06 +0530

    added functionality of get evidences on citizen sides

--- crimereporting-backend/src/main/java/site/crimereporting/dtos/CrimeReportDetailsDTO.java
+++ crimereporting-backend/src/main/java/site/crimereporting/dtos/CrimeReportDetailsDTO.java
@@ -13,6 +13,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CrimeReportDetailsDTO {
+	private Long crimeReportId;
 	private LocalDate crimeDate;
 	private String description;
 	
