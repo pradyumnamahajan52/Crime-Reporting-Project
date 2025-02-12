88caec22c987dd954ed7a5f1ffe4fb3870e6860f
Lalini <lalinisahusirri@gmail.com>
2025-02-12 11:13:32 +0530

    Add Crime Category in Police panel backend frontend comleted

--- crimereporting-backend/src/main/java/site/crimereporting/dtos/CrimeCategoryRequestDTO.java
+++ crimereporting-backend/src/main/java/site/crimereporting/dtos/CrimeCategoryRequestDTO.java
@@ -1,7 +1,13 @@
 package site.crimereporting.dtos;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
 public class CrimeCategoryRequestDTO {
 	
 	@NotBlank
